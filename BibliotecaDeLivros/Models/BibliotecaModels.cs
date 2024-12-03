670d676e9a98e7a73de5651b7b64cd88b2d1e985
Aluno <andre.lcordeiro2006@gmail.com>
2024-12-02 22:00:14 -0300

    BibliotecaFodastica

---
+++ BibliotecaDeLivros/Models/BibliotecaModels.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaDeLivros.Models
+{
+    public class BibliotecaModels
+    {
+        public int Id { get; set; }
+        public string ? Descricao { get; set; }
+        public string ? locacao { get; set; }
+        public string ? Titulo { get; set; }
+        public string ? Autor {  get; set; }
+        public int Ano {  get; set; }
+        public int Quantidade { get; set; }
+
+    }
+}
